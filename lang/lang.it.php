429d37b97350f506cb860b1877786a60f1ad4e10
Davide Grobberio <dadeg90@hotmail.it>
2013-07-24 18:47:13 +0200

    Italian translation
    Create italian translation.

---
+++ lang/lang.it.php
@@ -0,0 +1,21 @@
+<?php
+
+  $about = array(
+		'name' => 'Italian',
+		'author' => array(
+			'name' => 'Davide Grobberio',
+			'email' => 'davide@zaniniadv.it',
+			'website' => 'http://www.zaniniadv.it'
+		),
+		'release-date' => '2012-10-05'
+	);
+
+	/**
+	 * Multiple uploads
+	 */
+	$dictionary = array(
+
+		'Upload files' =>
+		'Carica files',
+
+	);
